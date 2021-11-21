95fe4fef6d3829e875df57ced22f8de7a36dc897
Tyler Woodfin <tyler@tylerwoodfin.com>
2021-11-21 16:37:53 -0600

    Significant refactoring; removed unused functions from original forked code, optimized function calls

--- bitcoin.py
+++ bitcoin.py
@@ -9,10 +9,9 @@ from inky import InkyPHAT
 import argparse
 from PIL import Image, ImageFont, ImageDraw
 import os
-from font_fredoka_one import FredokaOne
+from font_source_sans_pro import SourceSansProSemibold
 import datetime
 from inky.auto import auto
-import subprocess
 import sys
 import pwd
 
@@ -21,54 +20,38 @@ userDir = pwd.getpwuid( os.getuid() )[ 0 ]
 sys.path.insert(0, f'/home/{userDir}/Git/SecureData')
 import secureData
 
-#Variables
+# get current COIN price from Kraken API
+def getCoinPrice():
+    TICKER = API_ENDPOINT + "?pair=" + COIN
+    try:
+        r = requests.get(TICKER)
+        json_data = r.text
+        fj = json.loads(json_data)
+        lastpriceFloat = fj["result"][COIN]["c"][0]
+        return "{:.2f}".format(float(lastpriceFloat))
+    except requests.ConnectionError:
+        print("API - ERROR")
+
+# variables
 CURR_DIR = os.path.dirname(os.path.realpath(__file__)) + "/"
 RESOURCES = CURR_DIR + "resources/"
-TIME = datetime.datetime.now()
-COURIER_FONT = RESOURCES + "fonts/Courierprime.ttf"
 API_ENDPOINT = "https://api.kraken.com/0/public/Ticker"
-DP = "{:.2f}" #change the value for the required decimal places
+COIN = "XXBTZUSD"
+CURRENCYEXTRACT = "USD"
+CURRENCYSYMBOL = "$"
+CURRENT_PRICE = getCoinPrice()
 
 inky_display = inky_display = auto(ask_user=True, verbose=True)
 inky_display.set_border(inky_display.WHITE)
 
-# Return first task
-def getTopTask():
-    output = subprocess.check_output("ssh -t pi@192.168.1.123 -p 55701 'python3 /home/pi/Git/Tasks/tasks.py ls'", shell=True).decode(sys.stdout.encoding).strip()
-    output = output.splitlines()[0][1:].strip()
-    return output
-
-# Parsing flip arguments
+# parse flip arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--flip', '-f', type=str, help="true = screen is flipped")
 parser.add_argument('--pair', '-p', type=str, help="Enter currency pair")
 parser.add_argument('--debug', '-d', type=str, help="true = enable debug")
 args, _ = parser.parse_known_args()
 
-#Select currency based on currency pair input
-def findCurrency(currency):
-    if currency == "GBP":
-        return str("£")
-    elif currency == "USD":
-        return str("$")
-    elif currency == "EUR":
-        return str("€")
-    elif currency == "CAD":
-        return str("$")
-    elif currency == "AUD":
-        return str("$")
-        
-#If coin pair is entered, find correct currency
-if args.pair is not None:
-    COIN = str.upper(args.pair)
-    CURRENCYEXTRACT = str.upper(args.pair)[-3:]
-    CURRENCYSYMBOL = findCurrency(CURRENCYEXTRACT)
-else:
-    COIN = "XXBTZUSD"
-    CURRENCYEXTRACT = "USD"
-    CURRENCYSYMBOL = "$"
-
-#GET ERROR
+# check for errors
 def getError():
     TICKER = API_ENDPOINT + "?pair=" + COIN
     r = requests.get(TICKER)
@@ -79,59 +62,14 @@ def getError():
   
 if len(getError())!=0:
     print("Invalid coin pair: " + args.pair)
-
-# Get current COIN price from Kraken API
-def getCoinPrice():
-    TICKER = API_ENDPOINT + "?pair=" + COIN
-    try:
-        r = requests.get(TICKER)
-        json_data = r.text
-        fj = json.loads(json_data)
-        lastpriceFloat = fj["result"][COIN]["c"][0]
-        return DP.format(float(lastpriceFloat))
-    except requests.ConnectionError:
-        print("API - ERROR")
-    
-# Get low COIN price from Kraken API
-def getCoinPriceLow():
-    TICKER = API_ENDPOINT + "?pair=" + COIN
-    try:
-        r = requests.get(TICKER)
-        if r.status_code != 200:
-            print(r.status_code)
-        else:
-            json_data = r.text
-            fj = json.loads(json_data)
-            lowpriceFloat = fj["result"][COIN]["o"]
-            return DP.format(float(lowpriceFloat))
-    except requests.ConnectionError:
-        print("API - ERROR")
-    
-#Calculate the 24hour percentage change
-def percentUpDown():
-    if getCoinPrice() > getCoinPriceLow():
-        return "+"
-    else:
-        return ""
-                
-if len(getError())==0:            
-    PERCENTUPDOWN = str(percentUpDown())
-
-def percent(a, b) : 
-    result = int(((b - a) * 100) / a) 
-    return result 
-  
-if len(getError())==0: 
-    a, b = float(getCoinPriceLow()),float(getCoinPrice())
-    PERCENTAGE = percent(a, b)
       
-#Check for file, if it doesn't exist create, otherwise read file
+# check for file, if it doesn't exist create, otherwise read file
 file_exists = os.path.isfile(CURR_DIR + "/" + "previousprice")
 
 def updatePriceFile():
     f = open(CURR_DIR + "/" + "previousprice", "w")
-    f.write(getCoinPrice())
-    return float(getCoinPrice())
+    f.write(CURRENT_PRICE)
+    return float(CURRENT_PRICE)
     
 def previousPriceFile():
     if file_exists:
@@ -141,8 +79,8 @@ def previousPriceFile():
     else:
         print("Price file not found")
         f = open(CURR_DIR + "/" + "previousprice", "w")
-        f.write(getCoinPrice())
-        return float(getCoinPrice())
+        f.write(CURRENT_PRICE)
+        return float(CURRENT_PRICE)
         
 if len(getError())==0:
     PREVIOUS_PRICE = str(previousPriceFile())
@@ -151,8 +89,7 @@ if len(getError())==0:
     
 # Get price and format
 if len(getError())==0:
-    COINPRICELOW = float(getCoinPriceLow())
-    COINPRICE = float(getCoinPrice())
+    COINPRICE = float(CURRENT_PRICE)
     NUMBER_WITH_COMMAS = "{:,.2f}".format(COINPRICE)
 
 #Flip screen is false argument not passed
@@ -160,56 +97,55 @@ if args.flip != "false":
     inky_display.h_flip = True
     inky_display.v_flip = True
 
-# get BTC logo
-if len(getError())==0:
-    BTCLOGO = RESOURCES + "btc.png"
-    btcimg = Image.open(BTCLOGO)
+# load SecureData
+planty_status = secureData.variable('PLANTY_STATUS')
+weather_data = json.loads(secureData.variable("WEATHER_DATA"))
 
-#Compare current and previous prices & build price to display
+# load images
 if len(getError())==0:
-    if (float(COINPRICE) >= float(PREVIOUS_PRICE)):
-        print("Going UP")
-        COINPRICE = CURRENCYSYMBOL + NUMBER_WITH_COMMAS
-        ICON = RESOURCES + "icon-up.png"
-        iconimg = Image.open(ICON)
-    else:
-        print("Going DOWN")
-        COINPRICE = CURRENCYSYMBOL + NUMBER_WITH_COMMAS
-        print(COINPRICE)
-        ICON = RESOURCES + "icon-down.png"
-        iconimg = Image.open(ICON)
+    img_btc = Image.open(RESOURCES + "btc.png")
+    img_plant_inside = Image.open(RESOURCES + "icon-plant-inside.png")
+    img_plant_outside = Image.open(RESOURCES + "icon-plant-outside.png")
+    img_plant_unknown = Image.open(RESOURCES + "icon-plant-unknown.png")
+    img_weather = Image.open(RESOURCES + f"weather/{weather_data['current_conditions_icon']}.png")
 
+# load price
 if len(getError())==0:
-    if args.debug == "true":
-        print("The selected pair is " + COIN)
-        print("The selected currency " + CURRENCYSYMBOL + "(" + CURRENCYEXTRACT + ")")
-        print("The price has changed " + PERCENTUPDOWN + str(PERCENTAGE) + "% in last 24h's")
-        print("The low price is " + CURRENCYSYMBOL + "{:,}".format(COINPRICELOW))
-        print("The previous price is " + CURRENCYSYMBOL + PREVIOUS_PRICE_COMMAS)
-        print("The current price is " + CURRENCYSYMBOL + NUMBER_WITH_COMMAS)
-        print("The ICON is " + ICON)
-        
+    COINPRICE = f"${NUMBER_WITH_COMMAS}"
+    print(COINPRICE)        
 
-# Create a new canvas to draw on
+# create backdrop
 img = Image.open(RESOURCES + "backdrop.png").resize(inky_display.resolution)
 draw = ImageDraw.Draw(img)
 
-# load the fonts, and text size
-font = ImageFont.truetype(FredokaOne, 24)
-fontTopTask = ImageFont.truetype(FredokaOne, 35)
-font3 = ImageFont.truetype(COURIER_FONT, 50)
+# load fonts
+font_baseline = ImageFont.truetype(SourceSansProSemibold, 24)
+font_header = ImageFont.truetype(SourceSansProSemibold, 35)
+font_price = ImageFont.truetype(RESOURCES + "fonts/Courierprime.ttf", 40)
+font_price_label = ImageFont.truetype(SourceSansProSemibold, 20)
+font_temperature = ImageFont.truetype(SourceSansProSemibold, 95)
+font_divider = ImageFont.truetype(SourceSansProSemibold, 70)
 
-# Text to display and location
+# add elements to backdrop
 if len(getError())==0:
-    # print(getTopTask())
-    img.paste(btcimg, (0, 25))
-    draw.text((70, 25), str(COINPRICE), inky_display.RED, font=font3)
-    draw.text((5, 70), f"Updated at {TIME.strftime('%Y-%m-%d %H:%M:%S')}", inky_display.BLACK, font=font) 
+    draw.text((20, 25), f"BTC {str(COINPRICE)}", inky_display.RED, font=font_price)
+    draw.text((28, 70), f"Updated at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}", inky_display.BLACK, font=font_baseline) 
     
-    draw.text((0, 130), f"Planty Status: {secureData.variable('PLANTY_STATUS')}side", inky_display.YELLOW, font=fontTopTask)
-else:
-    draw.text((20, 25), "INVALID PAIR", inky_display.RED, font=font3)
+    draw.text((28, 160), f"{weather_data['current_temperature']}°", inky_display.BLACK, font=font_temperature)
+    img.paste(img_weather, (170,190))
+    
 
-# Display the text
+    draw.text((265, 175), "|", inky_display.RED, font=font_divider)
+    if planty_status == 'in':
+        img.paste(img_plant_inside, (300, 190))
+    elif planty_status == 'out':
+        img.paste(img_plant_outside, (300,190))
+    else:
+        img.paste(img_plant_unknown, (300,190))
+
+else:
+    draw.text((20, 25), "INVALID PAIR", inky_display.RED, font=font_price)
+
+# display
 inky_display.set_image(img)
 inky_display.show()
