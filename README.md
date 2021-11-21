0306dbc0aef38b05d7670305366fd48262e86ce6
Tyler Woodfin <tyler@tylerwoodfin.com>
2021-11-21 17:23:19 -0600

    Improved ReadMe; improved icons; renamed bitcoin.py to main.py

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 Little experiments for my Inky wHAT e-ink display (https://thepihut.com/products/inky-what-epaper-eink-epd)
 
 Source for Bitcoin tracker: Naviavia - https://github.com/naviavia/bitcoin_ticker
+
+## Dependencies
+This project is not self-sustained and is published for academic purposes rather than "cloning and running". It relies on certain files from the dependencies below.
+    
+- [Inky wHAT e-ink display](https://thepihut.com/products/inky-what-epaper-eink-epd)
+- [Secure Data](https://github.com/tylerjwoodfin/SecureData)
+    - Used to store project data across multiple repos and provides easy file access
+- Something running that populates the files mentioned in `main.py`, such as [Raspberry Pi Tools](https://github.com/tylerjwoodfin/RaspberryPI-Tools) and a crontab that runs the weather scripts within
+- [rclone](https://rclone.org/install/) (optional)
+    - Used to sync data between local devices and cloud repos, such as Dropbox
+
+## Sources
+[Bitcoin Ticker from Naviavia](https://github.com/naviavia/bitcoin_ticker)
+    - "soft-forked" from this repo as a starting point, then modified heavily to fit my needs
---
+++ main.py
@@ -0,0 +1,94 @@
+# /usr/bin/python3
+
+import requests
+import json
+import argparse
+from PIL import Image, ImageFont, ImageDraw
+import os
+from font_source_sans_pro import SourceSansProSemibold
+import datetime
+from inky.auto import auto
+import sys
+import pwd
+
+userDir = pwd.getpwuid( os.getuid() )[ 0 ]
+
+sys.path.insert(0, f'/home/{userDir}/Git/SecureData')
+import secureData
+
+# variables
+source_directory = os.path.dirname(os.path.realpath(__file__)) + "/"
+source_directory_resources = source_directory + "resources/"
+
+inky_display = inky_display = auto(ask_user=True, verbose=True)
+inky_display.set_border(inky_display.WHITE)
+
+# parse flip arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--flip', '-f', type=str, help="true = screen is flipped")
+parser.add_argument('--pair', '-p', type=str, help="Enter currency pair")
+parser.add_argument('--debug', '-d', type=str, help="true = enable debug")
+args, _ = parser.parse_known_args()
+
+# get current BTC price from Kraken API
+def getCoinPrice():
+    endpoint = "https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD"
+    try:
+        r = requests.get(endpoint)
+        json_data = r.text
+        fj = json.loads(json_data)
+        lastpriceFloat = fj["result"]["XXBTZUSD"]["c"][0]
+        return "{:.2f}".format(float(lastpriceFloat))
+    except requests.ConnectionError:
+        print("API - ERROR")
+
+#Flip screen is false argument not passed
+if args.flip != "false":
+    inky_display.h_flip = True
+    inky_display.v_flip = True
+
+# load SecureData
+planty_status = secureData.variable('PLANTY_STATUS')
+weather_data = json.loads(secureData.variable("WEATHER_DATA"))
+
+# load images
+img_btc = Image.open(source_directory_resources + "btc.png")
+img_plant_inside = Image.open(source_directory_resources + "icon-plant-inside.png")
+img_plant_outside = Image.open(source_directory_resources + "icon-plant-outside.png")
+img_plant_unknown = Image.open(source_directory_resources + "icon-plant-unknown.png")
+img_weather = Image.open(source_directory_resources + f"weather/{weather_data['current_conditions_icon']}.png")
+
+# create backdrop
+img = Image.open(source_directory_resources + "backdrop.png").resize(inky_display.resolution)
+draw = ImageDraw.Draw(img)
+
+# load fonts
+font_baseline = ImageFont.truetype(SourceSansProSemibold, 24)
+font_header = ImageFont.truetype(SourceSansProSemibold, 35)
+font_price = ImageFont.truetype(source_directory_resources + "fonts/Courierprime.ttf", 40)
+font_price_label = ImageFont.truetype(SourceSansProSemibold, 20)
+font_temperature = ImageFont.truetype(SourceSansProSemibold, 95)
+font_divider = ImageFont.truetype(SourceSansProSemibold, 70)
+
+# add elements to backdrop
+try:
+    draw.text((20, 25), f"BTC ${str('{:,.2f}'.format(float(getCoinPrice())))}", inky_display.RED, font=font_price)
+    draw.text((28, 70), f"Updated at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}", inky_display.BLACK, font=font_baseline) 
+    
+    draw.text((28, 160), f"{weather_data['current_temperature']}°", inky_display.BLACK, font=font_temperature)
+    img.paste(img_weather, (170,190))
+    
+
+    draw.text((265, 175), "|", inky_display.RED, font=font_divider)
+    if planty_status == 'in':
+        img.paste(img_plant_inside, (300, 190))
+    elif planty_status == 'out':
+        img.paste(img_plant_outside, (300,190))
+    else:
+        img.paste(img_plant_unknown, (300,190))
+except Exception as e:
+    draw.text((20, 25), f"Error:\n{e}", inky_display.RED, font=font_price)
+
+# display
+inky_display.set_image(img)
+inky_display.show()
